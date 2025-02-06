aa8cdd974a2e7007381e16130ee51a1a1196da20
Грищенко Роман Сергійович <vt231_grs@student.ztu.edu.ua>
2025-02-06 22:42:18 +0200

    povernuv stariy code

--- js3.js
+++ js3.js
@@ -1,30 +1,29 @@
-let text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea earum incidunt minima nihil. Blanditiis consectetur cumque debitis delectus dolor dolorem dolorum, eos laboriosam, modi nemo, quasi quos sint voluptatibus!`;
-
-
-let newS = (text[12] || '') + (text[6] || '') + (text[18] || '') + (text[25] || '');
+let text = Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea earum incidunt minima nihil. Blanditiis consectetur cumque
+debitis delectus dolor dolorem dolorum, eos laboriosam, modi nemo, quasi quos sint voluptatibus!;
+//1
+let newS = text[12] + text[6] + text[18] + text[25];
 console.log(newS);
-
-
+//2
 let low = text.toLowerCase();
 console.log(low);
-
-
-let repeat = [...text.matchAll(/in/g)].map(match => match.index);
+//3
+let repeat = [];
+let rep = text.indexOf("in");
+while (rep !== -1)
+{
+    repeat.push(rep);
+    rep = text.indexOf("in", rep + 1);
+}
 console.log(repeat);
-
-
-let array = text.split(/\s+/);
+//4
+let array = text.split(' ');
 console.log(array);
-
-
+//5
 let rev = text.split('').reverse().join('');
 console.log(rev);
-
-
+//6
 function ucFirst(txt) {
-    if (!txt) return '';
-    return txt.charAt(0).toUpperCase() + txt.slice(1);
+    let newTxt = txt.charAt(0).toUpperCase() + txt.slice(1);
+    return newTxt;
 }
-
 console.log(ucFirst("some text"));
-console.log(ucFirst(""));
